35453f0df16d2a6b1ff5154449f9bde2cbe396fc
Peskov Sergey <peskovdev@proton.me>
2023-07-09 03:50:16 +0600

    Add raises through parametrize. Groupes tests in class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 class Calculator:
     def sum(self, x: int | float, y: int | float) -> int | float:
+        if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+            raise TypeError("Both arguments must be numeric")
         return x + y
 
     def divide(self, x: int | float, y: int | float) -> int | float:
+        if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+            raise TypeError("Both arguments must be numeric")
+        if y == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         return x / y
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,30 @@
+import pytest
+from contextlib import nullcontext as does_not_raise
 from app.main import Calculator
 
-import pytest
 
+class TestCalculator:
+    @pytest.mark.parametrize(
+        "x, y, res, expectation",
+        [
+            (-1, 1, 0, does_not_raise()),
+            (5, "-1", 4, pytest.raises(TypeError)),
+            ("2", "1", "21", pytest.raises(TypeError)),
+        ],
+    )
+    def test_sum(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().sum(x, y) == res
 
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (-1, 1, 0),
-        (5, -1, 4),
-    ]
- )
-def test_sum(x, y, res):
-    assert Calculator().sum(x, y) == res
-
-
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 0.5),
-        (5, -1, -5),
-    ]
- )
-def test_divide(x, y, res):
-    assert Calculator().divide(x, y) == res
+    @pytest.mark.parametrize(
+        "x, y, res, expectation",
+        [
+            (1, 2, 0.5, does_not_raise()),
+            (5, "-1", -5, pytest.raises(TypeError)),
+            ("5", "-1", -5, pytest.raises(TypeError)),
+            (5, 0, 0, pytest.raises(ZeroDivisionError)),
+        ],
+    )
+    def test_divide(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().divide(x, y) == res
